26fe5c6aca7cdd3d363cee991daba4ad26b1ce68
Naiko <glclaritas@gmail.com>
2025-07-17 22:24:41 +0630

    reworked header file and main code

--- src/main.c
+++ src/main.c
@@ -1,18 +1,23 @@
-#include "./cpuctl.h"
-#include "./fanctl.h"
+#include <stdio.h>
 #include <string.h>
+#include "./mode.h"
 
 int main(int argc, char *argv[]){
     if ( argc != 2 ) {
         printf("\nUsage: command mode\n");
-        printf("\nAvailable modes: -cpumodetoggle, -fanmodetoggle");
-        printf("\nExample: asusctl -cpumodetoggle\n");
-        return 1;
+        printf("\nAvailable modes:  --cpumodetoggle, --fanmodetoggle");
+        printf("\n                  --display");
+        printf("\nExample: asusctl --cpumodetoggle\n");
+        printf("Need Nerd Fonts.\n");
     }
-    if ( strcmp(argv[1], "-cpumodetoggle") == 0 ) {
+    if ( strcmp(argv[1], "--cpumodetoggle") == 0 ) {
         cpumode_toggle();
-    }else if ( strcmp(argv[1], "-fanmodetoggle") == 0 ) {
+    }else if ( strcmp(argv[1], "--fanmodetoggle") == 0 ) {
         fanmode_toggle();
+    }else if ( strcmp(argv[1], "--display") == 0 ) {
+        const char *cpu_display[]={"L","M","H"};
+        const char *fan_display[]={"󱑳","󱑴","󱑲"};
+        printf("%s %s\n", cpu_display[cpumode_id()], fan_display[fanmode_id()]);
     }
     return 0;
 }
---
+++ src/mode.h
@@ -0,0 +1,10 @@
+#ifndef MODE_H
+#define MODE_H
+
+int fanmode_toggle(void);
+int fanmode_id(void);
+
+int cpumode_toggle(void);
+int cpumode_id(void);
+
+#endif
